7f7fdf49df81007ca6f81948f1e6a38d55893197
Matths-P <matthspinheiro@gmail.com>
2024-05-08 00:31:26 -0300

    Add novos métodos para seleção de candidato

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,34 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
-		analisarCandidato(1900.0);
-		analisarCandidato(2200.0);
-		analisarCandidato(2000.0);
+		selecaoCandidatos();
+	}
+
+	static void selecaoCandidatos() {
+		String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA","DANIELA", "JORGE" };
+
+		int candidatosSelecionados = 0;
+		int candidatosAtual = 0;
+		double salarioBase = 2000.0;
+		
+		while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+			String canditado = candidatos[candidatosAtual];
+			double salarioPretendido = valorPretendido();
+
+			System.out.println("O candidato " + canditado + " solicitou esse valor pretendido: R$ " + String.format("%.2f", salarioPretendido));
+			if (salarioBase >= salarioPretendido) {
+				System.out.println("O candidato " + canditado + " foi selecionado para a vaga");
+				candidatosSelecionados++;
+			}
+			candidatosAtual++;
+		}
+	}
+
+	static double valorPretendido() {
+		return ThreadLocalRandom.current().nextDouble(1800, 2200);
 	}
 
 	static void analisarCandidato(double salarioPretendido) {
