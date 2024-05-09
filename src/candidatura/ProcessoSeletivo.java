95e13b90f8b1e68bd6b51f735eafd46a3e2bfdea
Matths-P <matthspinheiro@gmail.com>
2024-05-09 03:56:16 -0300

    Add método para entrar em contato com candidatos selecionados + método auxiliar para simular número de ligações realizadas

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,5 +1,6 @@
 package candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
@@ -8,7 +9,9 @@ public class ProcessoSeletivo {
 		System.out.println();
 		System.out.println();
         imprimirSelecionados(candidatosSelecionados);
-
+        System.out.println();
+        System.out.println();
+        entrandoEmContato(candidatosSelecionados);
 	}
 	
 	static String[] selecaoCandidatos() {
@@ -19,6 +22,8 @@ public class ProcessoSeletivo {
 		double salarioBase = 2000.0;
 		String[] candidatosSelecionadosArray = new String[5];
 
+		System.out.println("CANDIDATOS NO PROCESSO:");
+		
 		while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
 			String canditado = candidatos[candidatosAtual];
 			double salarioPretendido = valorPretendido();
@@ -35,7 +40,7 @@ public class ProcessoSeletivo {
 	}
 	
 	static void imprimirSelecionados(String [] candidatosSelecionados) {
-		System.out.println("Candidatos selecionados:");
+		System.out.println("CANDIDATOS SELECIONADOS:");
 		
 		for (int candidatoAtual = 0; candidatoAtual < candidatosSelecionados.length; candidatoAtual++) {
 			if (candidatosSelecionados[candidatoAtual] != null) {
@@ -43,7 +48,34 @@ public class ProcessoSeletivo {
 			}
 		}
 	}
+	
+	static boolean atender() {
+		return new Random().nextInt(3)==1;
+	}
 
+	static void entrandoEmContato(String[] candidatosSelecionados) {
+		System.out.println("STATUS LIGAÇÃO:");
+		for (String candidato : candidatosSelecionados) {
+			int tentativasRealizadas = 1;
+			boolean continuarTentando = true;
+			boolean atendeu = false;
+			do {
+				atendeu = atender();
+				continuarTentando = !atendeu;
+				if (continuarTentando) {
+					tentativasRealizadas++;
+				}
+
+			} while (continuarTentando && tentativasRealizadas < 3);
+
+			if (atendeu) {
+				System.out.println(candidato + ": Conseguimos contato na " + tentativasRealizadas + " tentativa.");
+			} else {
+				System.out.println(candidato + ": Não conseguimos contato. Foram realizadas " + tentativasRealizadas + " tentativas.");
+			}
+		}
+	}
+	
 	static double valorPretendido() {
 		return ThreadLocalRandom.current().nextDouble(1800, 2200);
 	}
@@ -58,4 +90,5 @@ public class ProcessoSeletivo {
 			System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
 		}
 	}
+	
 }
