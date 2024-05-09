1a474c242117b540baebe878c155b7ea98ae7f54
Matths-P <matthspinheiro@gmail.com>
2024-05-09 00:57:02 -0300

    Add imprimirCandidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -4,16 +4,21 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
-		selecaoCandidatos();
-	}
+		String [] candidatosSelecionados = selecaoCandidatos();
+		System.out.println();
+		System.out.println();
+        imprimirSelecionados(candidatosSelecionados);
 
-	static void selecaoCandidatos() {
-		String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA","DANIELA", "JORGE" };
+	}
+	
+	static String[] selecaoCandidatos() {
+		String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE" };
 
 		int candidatosSelecionados = 0;
 		int candidatosAtual = 0;
 		double salarioBase = 2000.0;
-		
+		String[] candidatosSelecionadosArray = new String[5];
+
 		while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
 			String canditado = candidatos[candidatosAtual];
 			double salarioPretendido = valorPretendido();
@@ -21,9 +26,21 @@ public class ProcessoSeletivo {
 			System.out.println("O candidato " + canditado + " solicitou esse valor pretendido: R$ " + String.format("%.2f", salarioPretendido));
 			if (salarioBase >= salarioPretendido) {
 				System.out.println("O candidato " + canditado + " foi selecionado para a vaga");
+				candidatosSelecionadosArray[candidatosSelecionados] = canditado;
 				candidatosSelecionados++;
 			}
 			candidatosAtual++;
+		}
+		return candidatosSelecionadosArray;
+	}
+	
+	static void imprimirSelecionados(String [] candidatosSelecionados) {
+		System.out.println("Candidatos selecionados:");
+		
+		for (int candidatoAtual = 0; candidatoAtual < candidatosSelecionados.length; candidatoAtual++) {
+			if (candidatosSelecionados[candidatoAtual] != null) {
+				System.out.println("O candidato de número " + (candidatoAtual+1) + " é " + candidatosSelecionados[candidatoAtual]);
+			}
 		}
 	}
 
